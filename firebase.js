417e8bed92ab8a30aa783413054b68698f23b26c
Hongzhe Duan <10190811+hongzheduan@users.noreply.github.com>
2026-04-29 23:20:13 -0400

    add login password

---
+++ firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDVX4hPWgY_JK3VyXDjZapeki6Mm-tvw80",
+  authDomain: "baizora.firebaseapp.com",
+  projectId: "baizora",
+  appId: "1:1046306710691:web:b871cdedee87e9e6574b16"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
